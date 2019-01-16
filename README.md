f3a4c244a799817295d2f187088ca334aa81c297
Alexey <iwebinet@ya.ru>
2019-01-16 19:56:20 +0300

    Stage 4

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/ac85becec5d2442b142c/maintainability)](https://codeclimate.com/github/Iwebinet/project-lvl1-s412/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/ac85becec5d2442b142c/test_coverage)](https://codeclimate.com/github/Iwebinet/project-lvl1-s412/test_coverage)
 [![Build Status](https://travis-ci.org/Iwebinet/project-lvl1-s412.svg?branch=master)](https://travis-ci.org/Iwebinet/project-lvl1-s412)
+
+## Running game
+
+[![asciicast](https://asciinema.org/a/wtfRG47RCccYvOzZv6u65uLXg.svg)](https://asciinema.org/a/wtfRG47RCccYvOzZv6u65uLXg)
--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -2,6 +2,29 @@
 import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if number even otherwise answer "no".');
 
 const name = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${name}!`);
+
+const parity = number => {
+    console.log(`Question: ${number}`);
+    const reply = readlineSync.question('Your answer: ');
+    if (number % 2 === 0 && reply === 'yes' || number % 2 !== 0 && reply === 'no') {
+        console.log(`Correct!`);
+        return 1;
+    } else {
+        console.log(`'${reply}' is wrong answer ;(. Correct answer was '${number % 2 === 0 ? "yes" : "no"}'.`);
+        console.log(`Let's try again, ${name}!`);
+        return;
+    }
+};
+
+const number = k => k(Math.random() * (99 - 1 + 1)) + 1;
+if (parity(number(Math.floor)) === 1) {
+    if (parity(number(Math.floor)) === 1) {
+        if (parity(number(Math.floor)) === 1) {
+            console.log(`Congratulations, ${name}!`);
+        }
+    }
+}
